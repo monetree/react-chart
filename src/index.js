6f643382b555e656070b1a0e6accebc16bd31f83
soubhagya <soubhagya@recruitmaze.com>
2018-11-22 15:49:08 +0530

    adding changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const chartConfigs = {
+  type: 'Pie3D',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: {
+        "chart": {
+          "caption": "Recommended Portfolio Split",
+          "subCaption": "For a net-worth of $1M",
+          "showValues": "1",
+          "showPercentInTooltip": "0",
+          "numberPrefix": "$",
+          "enableMultiSlicing": "1",
+          "theme": "fusion"
+        },
+        "data": [
+          {
+            "label": "Equity",
+            "value": "300000"
+          },
+          {
+            "label": "Debt",
+            "value": "230000"
+          },
+          {
+            "label": "Bullion",
+            "value": "180000"
+          },
+          {
+            "label": "Real-estate",
+            "value": "270000"
+          },
+          {
+            "label": "Insurance",
+            "value": "20000"
+          }
+        ]
+      }
+};
+
+class Chart extends Component {
+  render () {
+    return <ReactFC {...chartConfigs} />;
+  }
+}
+
+ReactDOM.render(
+  <Chart />,
+  document.getElementById('root'),
+);
